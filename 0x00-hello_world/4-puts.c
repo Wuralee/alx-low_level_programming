225a19b61212200896e8b34a6c9e21a147dac4d0
wuralee <seseyonwhenu222@gmail.com>
2022-09-08 19:32:04 -0700

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
  #include <stdio.h>
 
  /**
- * Description: print the string in the puts function
- * 
+ * function main - print the variable in the puts function
+ *
+ * Description: use the main function
  * this program prints "Programming is like building a multilingual puzzle
  * Return: 0
-  */
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
