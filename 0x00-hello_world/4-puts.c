3d002e99642e834d8484b02ca838d30cb06ad9b6
wuralee <seseyonwhenu222@gmail.com>
2022-09-08 19:00:13 -0700

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
  #include <stdio.h>
 
  /**
- * function main - print the variable in the puts function
+ * main - print the variable in the puts function
  *
- *Description: use the main function
+ * Description: using the main function
  * program prints "Programming is like building a multilingual puzzle
  * return: 0
   */
