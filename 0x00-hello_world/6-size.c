eef99e4825065c1bdb1e8a4c8f07401a562b8b89
wuralee <seseyonwhenu222@gmail.com>
2022-09-08 20:55:07 -0700

    task6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,13 +10,13 @@
 int main(void)
 {
 	int i;
-	char c;
-	short s;
-	long li;
+	 char c;
+	  short s;
+	   long li;
 
-	printf("size of an int: %ld byte(s)\n", sizeof(i));
-	printf("size of a char: %ld byte(s)\n", sizeof(c));
-	printf("size of a short: %ld byte(s)\n", sizeof(s));
-	printf("size of a long: %ld byte(s)\n", sizeof(li));
+	 printf("size of an int: %ld byte(s)\n", sizeof(i));
+	  printf("size of a char: %ld byte(s)\n", sizeof(c));
+	   printf("size of a short: %ld byte(s)\n", sizeof(s));
+	    printf("size of a long: %ld byte(s)\n", sizeof(li);
         return (0);
 }
