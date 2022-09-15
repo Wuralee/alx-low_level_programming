fc0b3ce7fd71ac7c0160267072dce0a18fba2d75
wuralee <seseyonwhenu222@gmail.com>
2022-09-14 21:51:27 -0700

    task9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -2,11 +2,13 @@
 
 /**
  * times_table - prints the 9 times table ,starting with 0
- */
+ * @prod - is the result of the multiplied value
+ * @mult - is the mulipilication sign
+ * @num - is the number used to carry out the task
 
 void times_table(void)
 {
-	int num, mult, prod:
+	int num, mult, prod;
 
 		for (num = 0; num <= 9; num++)
 		{
