b21c2f9bb8ab6e912c0edbfa3f6d0c0ea88e6cc8
wuralee <seseyonwhenu222@gmail.com>
2022-09-14 19:32:45 -0700

    task2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * print_alphabet_10x - function to print abc ten times
+ * print_alphabet_x10 - function to print abc ten times
  *
  * Return: 0
  */
 
-void print_alphabet_10x(void)
+void print_alphabet_x10(void)
 {
 	char c, i;
 
