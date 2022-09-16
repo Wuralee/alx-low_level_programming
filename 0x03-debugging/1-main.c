2075ad4e88ae33124f4dc5be36a1643b88107d7d
wuralee <seseyonwhenu222@gmail.com>
2022-09-15 17:59:43 -0700

    task1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+
 /**
  * main - causes an infinite loop
  * Return: 0
@@ -19,6 +20,6 @@ int main(void)
 	/*}*/
 
 	printf("Infinite loop avoided! \\o/\n");
-
+	
 	return (0);
 }
