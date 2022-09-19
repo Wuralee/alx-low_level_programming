9b2fbc5fc7dc1d32483468578831ca3d80beb846
wuralee <seseyonwhenu222@gmail.com>
2022-09-19 16:53:13 -0700

    0. 98 Battery st.

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -11,10 +11,7 @@
 
 void reset_to_98(int *n)
 {
-	int n;
-
-	n = 98;
-	_putchar("%d\n", n);
+	*n = 98;
 
 	return (0);
 }
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -3,7 +3,7 @@
 
 #include <stdio.h>
 
-int _putchar(char c);
+_putchar;
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
