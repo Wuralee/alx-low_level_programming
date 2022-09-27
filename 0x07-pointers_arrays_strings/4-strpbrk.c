9945d6268efdf984bbc11c2b5a838bed8585fe93
wuralee <seseyonwhenu222@gmail.com>
2022-09-27 11:15:10 -0700

    search for a string

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strpbrk - print a function that searches
+ * a string for any of a set of bytes
+ * @s: string
+ * @accept: input
+ *
+ * Return: 0
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int b;
+
+	while (*s)
+	{
+		for (b = 0; accept[b]; b++)
+		{
+			if (*s == accept[b])
+				return (s);
+		}
+		s++;
+	}
+
+	return ('\0');
+}
